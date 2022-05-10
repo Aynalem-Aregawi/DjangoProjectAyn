3d7a2d4578f18a3560697e6413debf7d8fe150c6
Aynalem-Aregawi <61061329+Aynalem-Aregawi@users.noreply.github.com>
2022-05-10 14:04:04 +0300

    Update views.py

--- mywebsite/homepage/views.py
+++ mywebsite/homepage/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'Index.html')
+    return render(request, 'index.html')
 
 def intro(request):
     return render(request, 'Intro.html')
